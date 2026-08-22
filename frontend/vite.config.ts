f7b75d34cafe718d1c95195536a0266cfb588f31
srinivas-19-19 <gorkalsreenu10@gmail.com>
2026-08-22 11:26:32 +0530

    Update vite base URL for GitHub Pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MediQuee_Hospital/',
+  base: '/MediQuee_Hospital_Dashboard/',
   plugins: [react()],
   resolve: {
     alias: {
